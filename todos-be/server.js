76e1fc71172ee4ada420690e73d02acfac164559
nanachi10101000 <90586292+nanachi10101000@users.noreply.github.com>
2021-10-30 14:27:37 +0800

    10/30下午抓資料

---
+++ todos-be/server.js
@@ -0,0 +1,107 @@
+const express = require("express");
+const path = require("path");
+let app = express(); // application
+const mysql = require("mysql");
+require('dotenv').config();
+const Promise = require("bluebird");
+
+let connection = mysql.createConnection({
+  
+  host: process.env["DB_HOST"], // 本機 127.0.0.1
+  // port: 3306, // 埠號 mysql 預設就是 3306
+  user: process.env["DB_USER"],
+  password: process.env["DB_PASS"],
+  database: process.env["DB_DATABASE"],
+});
+
+
+connection = Promise.promisifyAll(connection)
+
+app.use(express.static("static"));
+//localhost:3001/about.html
+
+// app.use(PATH, express.static(檔案夾))
+// express.static(檔案夾名稱) 是內建的中間件
+http: app.use("/static", express.static("static"));
+// http://localhost:3001/static/about.html
+
+// app.set 設定這個 application 的一些變數
+// views: 告訴 app view 的檔案夾是哪一個
+// view engine: 告訴 app 你用哪一種 view engine
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+
+// app.use 告訴 express 這裡有一個中間件(middleware)
+// middleware 只是一個函式，會有三個參數
+app.use((req, res, next) => {
+  console.log("我是 AAAA 中間件");
+
+  // 如果沒有 next，那就停在這裡
+  next();
+  // next 可以讓他往下一關前進
+  // 但是目前這個中間件「不需要」知道下一個知道
+});
+
+
+app.use((req, res, next) => {
+  let current = new Date();
+  console.log(`有人來訪問 at ${current.toISOString()}`);
+
+  // 完全不關心 next 是誰
+  // 只知道要給下一個
+  next();
+  // 低耦合
+});
+
+// 路由 route / router --> 其實也算是一種中間件
+// app.Method(Path, Handler)
+// Method: GET, POST, PUT, DELETE, PATCH, ...
+// Handler 是一個函式，會有兩個參數 request, response
+app.get("/", (req, res) => {
+  console.log("我是首頁");
+  // res.send("我是 Express 首頁");
+  // 告訴 express 這個路由要用的樣板檔案是哪一個
+  let data = {
+    name: "ashley",
+    job: "engineer",
+    cities: ["Taipei", "YiLan"],
+  };
+  res.render("index", data);
+});
+
+app.get("/member", (req, res, next) => {
+  console.log("我是會員頁 1");
+  // res.send("我是會員頁");
+  next();
+});
+
+app.get("/member", (req, res) => {
+  console.log("我是會員頁 2");
+  res.send("我是會員頁");
+});
+
+app.get("/api/todos", async (req, res) => {
+  let data = await connection.queryAsync("SELECT * FROM todos");
+  res.json(data);
+});
+
+// 職責切割
+
+// 這個中間件是負責做紀錄
+app.use((req, res, next) => {
+  console.log(`${req.url} 找不到路由`);
+  next();
+});
+
+// 既然會走到所有路由後面的這個中間件
+// 就表示前面所有路由中間件的 path 都比不到
+// --> 404 !!
+app.use((req, res, next) => {
+  console.log("我是路由後面的中間件");
+  res.status(404).send("Not Found");
+});
+
+// 3006 port
+app.listen(3006, () => {
+  console.log("express app 啟動了喔");
+});
